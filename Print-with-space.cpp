73ccfa98d6425abc1e27c76282846adebd677ae8
Manikanta Rayala <72917946+manikanta9430@users.noreply.github.com>
2022-02-28 22:46:03 +0530

    Create Print-with-space.cpp

---
+++ Print-with-space.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void utility(string a, string b){
+    cout<<a<<" "<<b<<endl;
+    
+}
+
+    
+int main() {
+	
+    int t ; cin>>t; 
+    while (t--) 
+    { 
+        string a, b;
+        cin>>a>>b;
+  
+        utility(a, b);
+    } 
+    return 0;
+}
