7f85edcfb6acc3f2d2615c51d990657e6e727b55
Manikanta Rayala <72917946+manikanta9430@users.noreply.github.com>
2022-02-28 22:26:54 +0530

    Create Input-integer.cpp

---
+++ Input-integer.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+    
+int main() {
+	
+	
+    int t; cin>>t; 
+        while(t--) {
+            
+            
+int myInput;
+
+cin>>myInput;
+
+cout<<myInput+10<<endl;
+
+        }
+}  
